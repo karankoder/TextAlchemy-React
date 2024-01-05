605ecafa1c8f9c80040b09535cab539dc4084c77
karankoder <karankr.das.cse22@iitbhu.ac.in>
2024-01-05 22:32:21 +0530

    Bug fix

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -4,10 +4,12 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default function Alert(props) {
     return (
-        props.alert && <div>
+        <div className="container" style={{height:'25px',width:'350px'}}>
+            {props.alert && <div>
             <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
                 <strong>{props.alert.msg}</strong> 
             </div>
+        </div>}
         </div>
     )
 }
